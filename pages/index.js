6ac13297277c252b8accd05d0f85d2a2be090d5a
heris <heri.s.arisman@gmail.com>
2023-01-02 15:43:49 +0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import RestaurantList from '../components/RestaurantList';
+import styles from '../styles/Home.module.css'
+
+export default function Home({restaurants}) {
+  return (
+    <div className='container'>
+      <RestaurantList restaurants={restaurants} />
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('https://restaurant-api.dicoding.dev/list');
+  const { restaurants } = await response.json();
+ 
+  return {
+    props: {
+      restaurants,
+    },
+  }
+}
